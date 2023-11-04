3f667f0d0d54adab3018703d203d7e43e20e9f9e
rjjm94 <rjjm94@gmail.com>
2023-11-03 20:34:00 -0400

    fast socks

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -2,10 +2,13 @@
 package proxy
 
 import (
+	"bufio"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"github.com/armon/go-socks5"
 	"github.com/sirupsen/logrus"
+	"io"
 	"net"
 	"sync/atomic"
 	"time"
@@ -62,6 +65,41 @@ func (m *ConnectionManager) Connect(ctx context.Context, network, address string
 // HandleConnection serves a connection with the given SOCKS5 server and tracks the number of successful and failed requests.
 func (m *ConnectionManager) HandleConnection(socksServer *socks5.Server, conn net.Conn) {
 	defer m.Close(conn) // Ensure the connection is closed when the goroutine exits
+
+	// Create a buffered reader for the connection
+	reader := bufio.NewReader(conn)
+
+	// Check for gzip header
+	header, err := reader.Peek(2)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"event":       "peek_header_error",
+			"local_addr":  conn.LocalAddr().String(),
+			"remote_addr": conn.RemoteAddr().String(),
+		}).Error("Failed to peek header: ", err)
+		return
+	}
+
+	// If the data is compressed, create a gzip reader
+	if header[0] == 0x1f && header[1] == 0x8b {
+		gzipReader, err := gzip.NewReader(reader)
+		if err != nil {
+			atomic.AddInt64(&m.totalFailedConnections, 1)
+			logrus.WithFields(logrus.Fields{
+				"event":       "gzip_reader_error",
+				"local_addr":  conn.LocalAddr().String(),
+				"remote_addr": conn.RemoteAddr().String(),
+			}).Error("Failed to create gzip reader: ", err)
+			return
+		}
+		defer gzipReader.Close()
+		reader = bufio.NewReader(gzipReader)
+	}
+
+	// Create a new connection from the buffered reader
+	conn = &connWithReader{conn, reader}
+
+	// Serve the connection
 	if err := socksServer.ServeConn(conn); err != nil {
 		atomic.AddInt64(&m.totalFailedConnections, 1)
 		logrus.WithFields(logrus.Fields{
@@ -94,9 +132,9 @@ func (m *ConnectionManager) GetTotalFailed() int64 {
 // It also updates the maxConcurrentConnections counter if necessary.
 func (m *ConnectionManager) AcceptConnection() {
 	atomic.AddInt64(&m.totalConnections, 1)
-	atomic.AddInt64(&m.currentConnections, 1)
-	if atomic.LoadInt64(&m.currentConnections) > atomic.LoadInt64(&m.maxConcurrentConnections) {
-		atomic.StoreInt64(&m.maxConcurrentConnections, atomic.LoadInt64(&m.currentConnections))
+	currentConnections := atomic.AddInt64(&m.currentConnections, 1)
+	if currentConnections > atomic.LoadInt64(&m.maxConcurrentConnections) {
+		atomic.StoreInt64(&m.maxConcurrentConnections, currentConnections)
 	}
 }
 
@@ -120,16 +158,6 @@ func (m *ConnectionManager) Close(conn net.Conn) {
 	}
 }
 
-// IncrementSuccessfulConnections increments the totalSuccessfulConnections counter.
-func (m *ConnectionManager) IncrementSuccessfulConnections() {
-	atomic.AddInt64(&m.totalSuccessfulConnections, 1)
-}
-
-// IncrementFailedConnections increments the totalFailedConnections counter.
-func (m *ConnectionManager) IncrementFailedConnections() {
-	atomic.AddInt64(&m.totalFailedConnections, 1)
-}
-
 // GetTotalFailedConnections returns the total number of failed connections.
 func (m *ConnectionManager) GetTotalFailedConnections() int64 {
 	return atomic.LoadInt64(&m.totalFailedConnections)
@@ -138,4 +166,14 @@ func (m *ConnectionManager) GetTotalFailedConnections() int64 {
 // GetTotalSuccessfulConnections returns the total number of successful connections.
 func (m *ConnectionManager) GetTotalSuccessfulConnections() int64 {
 	return atomic.LoadInt64(&m.totalSuccessfulConnections)
+}
+
+// connWithReader is a net.Conn that has its own reader.
+type connWithReader struct {
+	net.Conn
+	reader io.Reader
+}
+
+func (c *connWithReader) Read(b []byte) (n int, err error) {
+	return c.reader.Read(b)
 }
