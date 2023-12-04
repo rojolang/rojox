aad3ecea4840d26b5252eeff981c77b6931c3dd7
rjjm94 <rjjm94@gmail.com>
2023-12-04 02:23:14 -0400

    code op[timizations

--- satellite/main.go
+++ satellite/main.go
@@ -1,16 +1,12 @@
 package satellite
 
 import (
+	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/rojolang/rojox/proxy"
-	"github.com/rojolang/rojox/stats"
-	"github.com/rojolang/rojox/utils"
-	"github.com/sirupsen/logrus"
 	"net"
 	"net/http"
 	"os"
@@ -19,391 +15,223 @@ import (
 	"strings"
 	"syscall"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/rojolang/rojox/proxy"
+	"github.com/rojolang/rojox/stats"
+	"github.com/rojolang/rojox/utils"
+	"go.uber.org/zap"
 )
 
 const UXServerIP = "http://34.209.231.131:8080/register"
 
+var (
+	readTimeout  = 5 * time.Second
+	writeTimeout = 10 * time.Second
+)
+
 type SimpleDialer struct{}
 
-// Dial creates a network connection using the specified network, address, and context.
-// It prefers IPv6 and falls back to IPv4 on the usb0 interface for outgoing connections.
 func (d *SimpleDialer) Dial(ctx context.Context, network, address string) (net.Conn, error) {
-	logrus.Debug("Entering SimpleDialer.Dial method")
-
-	// Try to resolve the address to see if it has an IPv6 address.
 	host, port, err := net.SplitHostPort(address)
 	if err != nil {
-		logrus.WithError(err).Error("Failed to split network address")
 		return nil, err
 	}
 
 	ips, err := net.LookupIP(host)
 	if err != nil {
-		logrus.WithError(err).Error("Failed to look up IP for host")
 		return nil, err
 	}
 
-	var ipv6Addr net.IP
+	var dialAddr string
 	for _, ip := range ips {
 		if ip.To4() == nil && ip.IsGlobalUnicast() {
-			ipv6Addr = ip
+			dialAddr = net.JoinHostPort(ip.String(), port)
+			network = "tcp6"
 			break
 		}
 	}
 
-	var dialAddr string
-	if ipv6Addr != nil {
-		// Use the IPv6 address if available.
-		dialAddr = net.JoinHostPort(ipv6Addr.String(), port)
-		network = "tcp6"
-	} else {
-		// Fall back to the original address, which could be IPv4.
+	if dialAddr == "" {
 		dialAddr = address
 		network = "tcp4"
 	}
 
-	// Create a dialer without specifying LocalAddr to use the system's default routing
 	dialer := &net.Dialer{}
-
-	// Dial out using the system's default routing
 	conn, err := dialer.DialContext(ctx, network, dialAddr)
 	if err != nil {
-		logrus.WithError(err).Error("Failed to dial")
 		return nil, err
 	}
 
-	logrus.WithFields(logrus.Fields{
-		"localAddr":  conn.LocalAddr().String(),
-		"remoteAddr": conn.RemoteAddr().String(),
-	}).Info("Successfully established connection")
-	logrus.Debug("Exiting SimpleDialer.Dial method")
 	return conn, nil
 }
 
-func dialWithLocalAddr(ctx context.Context, network, address string, localIP net.IP) (net.Conn, error) {
-	localAddr := &net.TCPAddr{IP: localIP, Port: 0}
-	dialer := &net.Dialer{LocalAddr: localAddr}
-	return dialer.DialContext(ctx, network, address)
-}
-
-func getZeroTierIP() (string, error) {
-	logrus.Debug("Satellite getZeroTierIP function started")
+func getZeroTierIP(logger *zap.Logger) (string, error) {
 	cmd := exec.Command("zerotier-cli", "listnetworks")
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
-	err := cmd.Run() // Use Run instead of Output to capture both stdout and stderr
+
+	err := cmd.Run()
 	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"stderr": stderr.String(),
-			"stdout": stdout.String(),
-		}).Error("Error executing zerotier-cli listnetworks")
+		logger.Error("Failed to execute zerotier-cli command", zap.Error(err))
 		return "", err
 	}
 
 	output := stdout.String()
 	lines := strings.Split(output, "\n")
-	logrus.Info("ZeroTier networks: ", lines)
 
 	for _, line := range lines {
 		fields := strings.Fields(line)
 		if len(fields) > 8 && fields[2] == "fada62b0151e0f56" {
 			ipWithMask := fields[8]
 			ip := strings.Split(ipWithMask, "/")[0]
-			logrus.Info("ZeroTier IP found: ", ip)
 			return ip, nil
 		}
 	}
-	logrus.Debug("Satellite getZeroTierIP function finished")
+
+	logger.Error("ZeroTier IP not found")
 	return "", errors.New("ZeroTier IP not found")
 }
 
-// getUSB0Addresses returns the IPv6 and IPv4 addresses of the usb0 interface.
-func getUSB0Addresses() (ipv6Addr, ipv4Addr net.IP) {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get network interfaces")
-		return nil, nil
-	}
-
-	for _, iface := range ifaces {
-		if iface.Name != "usb0" {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			logrus.WithError(err).WithField("interface", iface.Name).Error("Failed to get addresses for interface")
-			return nil, nil
-		}
-
-		for _, addr := range addrs {
-			ipNet, ok := addr.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			ip := ipNet.IP
-			if ip.To4() != nil {
-				ipv4Addr = ip
-			} else if ip.To16() != nil && ip.IsGlobalUnicast() {
-				ipv6Addr = ip
-			}
-		}
-	}
-
-	return ipv6Addr, ipv4Addr
-}
-
 func Run() {
-	logrus.Debug("Satellite Run function started")
-	logrus.SetLevel(logrus.DebugLevel)
-	logrus.SetOutput(os.Stdout)
-	logrus.Info("Starting satellite main function")
+	logger, _ := zap.NewProduction()
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil {
+			logger.Error("Failed to sync logger", zap.Error(err))
+		}
+	}(logger)
 
-	// Create a new SimpleDialer instance which will be used by the ConnectionManager
 	dialer := &SimpleDialer{}
-	// Create a new ConnectionManager instance providing the SimpleDialer
 	manager := proxy.NewConnectionManager(dialer)
 
-	// Setup the SOCKS5 server with the custom Dial function from our SimpleDialer
 	socksServer, err := utils.SetupSocks5Server()
 	if err != nil {
-		logrus.WithFields(logrus.Fields{"context": "setting up SOCKS5 server"}).Fatal(err)
+		logger.Fatal("Failed to set up SOCKS5 server", zap.Error(err))
 	}
 
-	// Fetch ZeroTier IP instead of eth0 IP
-	zeroTierIPString, err := getZeroTierIP()
+	zeroTierIPString, err := getZeroTierIP(logger)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{"context": "getting ZeroTier IP"}).Fatal(err)
+		logger.Fatal("Failed to get ZeroTier IP", zap.Error(err))
 	}
 
-	// Parse the ZeroTier IP string into net.IP type
-	zeroTierIP := net.ParseIP(zeroTierIPString)
-	if zeroTierIP == nil {
-		logrus.WithFields(logrus.Fields{"context": "parsing ZeroTier IP"}).Fatal("Invalid ZeroTier IP")
+	if err := registerWithUXServer(UXServerIP, zeroTierIPString, logger); err != nil {
+		logger.Fatal("Failed to register with UX server", zap.Error(err))
 	}
 
-	if err := registerWithUXServer(UXServerIP); err != nil {
-		logrus.WithFields(logrus.Fields{"context": "registering with UX server"}).Fatal(err)
-	}
-
-	// Use ZeroTier IP when listening for connections
-	listener, err := utils.ListenForConnections(socksServer, zeroTierIP, manager)
+	listener, err := utils.ListenForConnections(socksServer, net.ParseIP(zeroTierIPString), manager)
 	if err != nil {
-		logrus.WithFields(logrus.Fields{"context": "listening for connections"}).Fatal(err)
+		logger.Fatal("Failed to listen for connections", zap.Error(err))
 	}
 
-	httpServer, err := utils.SetupHTTPServer()
-	if err != nil {
-		logrus.WithFields(logrus.Fields{"context": "setting up HTTP server"}).Fatal(err)
+	httpServer := &http.Server{
+		Addr:         ":8080",
+		Handler:      bufioHandler(promhttp.Handler(), logger),
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
 	}
 
-	http.Handle("/metrics-satellite", promhttp.Handler())
-
-	logrus.Info("Handling termination signals")
-	handleTerminationSignals(httpServer, listener)
-	logrus.Info("Termination signals handled")
-
-	logrus.Info("Starting HTTP server")
 	go func() {
-		if err := utils.StartHTTPServer(httpServer); err != nil {
-			logrus.WithFields(logrus.Fields{"context": "starting HTTP server"}).Fatal(err)
+		if err := httpServer.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatal("HTTP server failed", zap.Error(err))
 		}
 	}()
-	logrus.Info("HTTP server started")
 
-	logrus.Info("Starting PrintStats goroutine")
 	go stats.PrintStats(manager)
-	logrus.Info("PrintStats goroutine started")
+	handleTerminationSignals(httpServer, listener, logger)
+}
 
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-	<-quit
+// BufioResponseWriter embeds http.ResponseWriter and a bufio.Writer to enable buffered writing.
+type BufioResponseWriter struct {
+	http.ResponseWriter
+	writer *bufio.Writer
+	logger *zap.Logger
+}
 
-	if err := httpServer.Shutdown(context.Background()); err != nil {
-		logrus.WithFields(logrus.Fields{"context": "shutting down HTTP server"}).Error(err)
+// NewBufioResponseWriter creates a new BufioResponseWriter with the provided http.ResponseWriter and logger.
+func NewBufioResponseWriter(w http.ResponseWriter, logger *zap.Logger) *BufioResponseWriter {
+	return &BufioResponseWriter{
+		ResponseWriter: w,
+		writer:         bufio.NewWriter(w),
+		logger:         logger,
 	}
-	logrus.Debug("Satellite Run function finished")
 }
 
-func handleTerminationSignals(httpServer *http.Server, listener net.Listener) {
-	logrus.Debug("Satellite handleTerminationSignals function started")
+// Write uses the bufio.Writer to write.
+func (b *BufioResponseWriter) Write(data []byte) (int, error) {
+	return b.writer.Write(data)
+}
+
+// Flush ensures that any buffered data is flushed to the underlying ResponseWriter.
+func (b *BufioResponseWriter) Flush() {
+	if err := b.writer.Flush(); err != nil {
+		b.logger.Error("Failed to flush buffered writer", zap.Error(err))
+	}
+}
+
+// bufioHandler wraps an HTTP handler to provide buffered writing for responses.
+func bufioHandler(h http.Handler, logger *zap.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		bwResponseWriter := NewBufioResponseWriter(w, logger)
+		defer bwResponseWriter.Flush()
+
+		h.ServeHTTP(bwResponseWriter, r)
+	})
+}
+
+func handleTerminationSignals(httpServer *http.Server, listener net.Listener, logger *zap.Logger) {
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
 
-	go func() {
-		<-quit
+	<-quit
+	logger.Info("Received termination signal")
 
-		logrus.Info("Received termination signal")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-		if err := httpServer.Shutdown(context.Background()); err != nil {
-			logrus.WithFields(logrus.Fields{"context": "shutting down HTTP server"}).Error(err)
-		}
+	if err := httpServer.Shutdown(ctx); err != nil {
+		logger.Error("Error shutting down HTTP server", zap.Error(err))
+	}
 
-		if err := listener.Close(); err != nil {
-			logrus.WithFields(logrus.Fields{"context": "closing SOCKS5 server listener"}).Error(err)
-		}
-	}()
-	logrus.Debug("Satellite handleTerminationSignals function finished")
+	if err := listener.Close(); err != nil {
+		logger.Error("Error closing SOCKS5 server listener", zap.Error(err))
+	}
 }
 
-func registerWithUXServer(uxServerIP string) error {
-	logrus.Debug("Satellite registerWithUXServer function started")
+func registerWithUXServer(uxServerIP, zeroTierIP string, logger *zap.Logger) error {
 	for i := 0; i < 3; i++ {
-		ip, err := getZeroTierIP()
+		reqBody, err := json.Marshal(map[string]string{"ip": zeroTierIP})
 		if err != nil {
-			logrus.WithField("context", "getting ZeroTier IP").Error(err)
+			logger.Error("Failed to marshal registration request body", zap.Error(err))
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		logrus.Info("Registering IP: ", ip)
 
-		reqBody, err := json.Marshal(map[string]string{"ip": ip})
-		if err != nil {
-			logrus.WithField("context", "creating register request").Error(err)
-			time.Sleep(1 * time.Second)
-			continue
-		}
 		req, err := http.NewRequest("POST", uxServerIP, bytes.NewBuffer(reqBody))
-
 		if err != nil {
-			logrus.WithField("context", "creating new request").Error(err)
+			logger.Error("Failed to create new HTTP request for registration", zap.Error(err))
 			time.Sleep(1 * time.Second)
 			continue
 		}
 		req.Header.Set("Content-Type", "application/json")
 
-		logrus.WithField("request", req).Info("Sending registration request")
-
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			logrus.WithField("context", "sending register request").Error(err)
+			logger.Error("Failed to send registration request", zap.Error(err))
 			time.Sleep(1 * time.Second)
 			continue
 		}
-		defer resp.Body.Close()
+		resp.Body.Close()
 
-		logrus.WithField("response", resp).Info("Received registration response")
-
-		if resp.StatusCode != http.StatusOK {
-			err = fmt.Errorf("registration failed: status code %d", resp.StatusCode)
-			logrus.WithField("context", "register response").Error(err)
+		if resp.StatusCode == http.StatusOK {
+			logger.Info("Successfully registered with UX server", zap.String("ip", zeroTierIP))
+			return nil
+		} else {
+			logger.Error("Registration failed", zap.Int("statusCode", resp.StatusCode), zap.String("ip", zeroTierIP))
 			time.Sleep(1 * time.Second)
-			continue
 		}
-
-		logrus.Info("Successfully registered with UX server")
-		return nil
 	}
-	logrus.Debug("Satellite registerWithUXServer function finished")
+
 	return fmt.Errorf("registration failed after 3 attempts")
-}
-
-// getIPv6Address retrieves the preferred global unicast IPv6 address of the system.
-func getIPv6Address() (net.IP, error) {
-	logrus.Debug("Satellite getIPv6Address function started")
-	logrus.Info("Retrieving global unicast IPv6 address for usb0")
-	iface, err := net.InterfaceByName("usb0")
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get usb0 network interface")
-		return nil, err
-	}
-
-	addrs, err := iface.Addrs()
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get addresses for interface usb0")
-		return nil, err
-	}
-
-	for _, addr := range addrs {
-		ipNet, ok := addr.(*net.IPNet)
-		if !ok {
-			continue
-		}
-		ip := ipNet.IP
-		if ip.To4() == nil && ip.IsGlobalUnicast() {
-			// Found a global unicast IPv6 address.
-			logrus.WithField("ipv6", ip.String()).Info("Global unicast IPv6 address found for usb0")
-			return ip, nil
-		}
-	}
-	logrus.Debug("Satellite getIPv6Address function finished")
-	return nil, errors.New("usb0 interface has no global unicast IPv6 address")
-}
-
-func getUSB0IPv6() (net.IP, error) {
-	ifaces, err := net.Interfaces()
-	logrus.Debug("Entering getUSB0IPv6 function")
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get network interfaces")
-		return nil, err
-	}
-
-	for _, iface := range ifaces {
-		if iface.Name != "usb0" {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			logrus.WithError(err).WithField("interface", iface.Name).Error("Failed to get addresses for interface")
-			return nil, err
-		}
-
-		for _, addr := range addrs {
-			ipNet, ok := addr.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			ip := ipNet.IP
-			if ip.To4() == nil && ip.IsGlobalUnicast() {
-				logrus.WithFields(logrus.Fields{
-					"interface": "usb0",
-					"ipv6":      ip.String(),
-				}).Info("IPv6 address found for usb0")
-				return ip, nil
-			}
-		}
-	}
-	logrus.Debug("Exiting getUSB0IPv6 function")
-	return nil, fmt.Errorf("usb0 interface not found or has no global unicast IPv6 address")
-}
-
-func getUSB0IPv4() (net.IP, error) {
-	ifaces, err := net.Interfaces()
-	logrus.Debug("Entering getUSB0IPv4 function")
-	if err != nil {
-		logrus.WithError(err).Error("Failed to get network interfaces")
-		return nil, err
-	}
-
-	for _, iface := range ifaces {
-		if iface.Name != "usb0" {
-			continue
-		}
-
-		addrs, err := iface.Addrs()
-		if err != nil {
-			logrus.WithError(err).WithField("interface", iface.Name).Error("Failed to get addresses for interface")
-			return nil, err
-		}
-
-		for _, addr := range addrs {
-			ipNet, ok := addr.(*net.IPNet)
-			if !ok {
-				continue
-			}
-			ip := ipNet.IP
-			// Check if the IP is an IPv4 address.
-			if ipv4 := ip.To4(); ipv4 != nil {
-				logrus.WithFields(logrus.Fields{
-					"interface": "usb0",
-					"ipv4":      ipv4.String(),
-				}).Info("IPv4 address found for usb0")
-				return ipv4, nil // Return the IPv4 address.
-			}
-		}
-	}
-	logrus.Debug("Exiting getUSB0IPv4 function")
-	return nil, fmt.Errorf("usb0 interface not found or has no IPv4 address")
 }
--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"errors"
 	"github.com/sirupsen/logrus"
 	"io"
@@ -9,17 +10,30 @@ import (
 	"time"
 )
 
-type LoadBalancer struct {
-	satellites []string
-	index      int
-	mu         sync.Mutex
-	wg         sync.WaitGroup
+type SatelliteStatus struct {
+	IP              string
+	ActiveConns     int
+	LastHealthCheck time.Time
+	Healthy         bool
 }
 
-// NewLoadBalancer creates a new LoadBalancer instance.
-func NewLoadBalancer() *LoadBalancer {
-	lb := &LoadBalancer{}
-	logrus.WithField("loadBalancer", lb).Info("Creating new LoadBalancer")
+type LoadBalancer struct {
+	satellites   []*SatelliteStatus
+	mu           sync.RWMutex // Use RWMutex for read/write locking
+	wg           sync.WaitGroup
+	connChan     chan net.Conn // Buffered channel for handling connections
+	shutdownChan chan struct{} // Channel to signal shutdown
+}
+
+// NewLoadBalancer creates a new LoadBalancer instance with a buffered channel.
+func NewLoadBalancer(bufferSize int, healthCheckInterval time.Duration) *LoadBalancer {
+	lb := &LoadBalancer{
+		connChan:     make(chan net.Conn, bufferSize),
+		shutdownChan: make(chan struct{}),
+	}
+	logrus.Info("Creating new LoadBalancer")
+	go lb.handleConnections()
+	go lb.scheduleHealthChecks(healthCheckInterval)
 	return lb
 }
 
@@ -29,84 +43,178 @@ func (lb *LoadBalancer) RegisterSatellite(zeroTierIP string) {
 		return
 	}
 
-	logrus.WithField("zeroTierIP", zeroTierIP).Info("Registering satellite") // Log the IP being registered
-	lb.mu.Lock()
-	lb.satellites = append(lb.satellites, zeroTierIP)
-	lb.mu.Unlock()
-	logrus.WithField("zeroTierIP", zeroTierIP).Info("Registered new satellite")
-	logrus.WithField("satellites", lb.satellites).Info("Current satellites") // Print the current list of satellites
-	logrus.WithField("loadBalancer", lb).Info("Current LoadBalancer")        // Print the address of the LoadBalancer instance
-}
-
-// NextSatellite returns the next satellite IP address.
-func (lb *LoadBalancer) NextSatellite() (string, error) {
 	lb.mu.Lock()
 	defer lb.mu.Unlock()
 
-	// If there are no satellites registered, return an error
-	if len(lb.satellites) == 0 {
-		err := errors.New("no satellites registered")
-		logrus.WithField("satellites", lb.satellites).Info("Satellites at the time of error") // Print the current list of satellites at the time of error
-		logrus.Error(err)
-		return "", err
+	for _, satellite := range lb.satellites {
+		if satellite.IP == zeroTierIP {
+			logrus.WithField("zeroTierIP", zeroTierIP).Info("Satellite already registered")
+			return
+		}
 	}
 
-	zeroTierIP := lb.satellites[lb.index]
-	lb.index = (lb.index + 1) % len(lb.satellites)
-
-	logrus.WithField("zeroTierIP", zeroTierIP).Info("Selected next satellite")
-	logrus.WithField("satellites", lb.satellites).Info("Current satellites after selection") // Print the current list of satellites after selection
-	logrus.WithField("loadBalancer", lb).Info("Current LoadBalancer after selection")        // Print the address of the LoadBalancer instance after selection
-
-	return zeroTierIP, nil
+	newSatellite := &SatelliteStatus{
+		IP:              zeroTierIP,
+		ActiveConns:     0,
+		LastHealthCheck: time.Now(),
+		Healthy:         true,
+	}
+	lb.satellites = append(lb.satellites, newSatellite)
+	logrus.WithField("zeroTierIP", zeroTierIP).Info("Registered new satellite")
 }
 
-// HandleConnection handles an incoming connection.
-func (lb *LoadBalancer) HandleConnection(conn net.Conn) {
-	logrus.WithField("remote_addr", conn.RemoteAddr().String()).Info("Handling connection")
-	defer func() {
-		logrus.WithField("remote_addr", conn.RemoteAddr().String()).Info("Closing connection")
-		conn.Close()
-	}()
+// performHealthChecks runs health checks on all satellites concurrently.
+func (lb *LoadBalancer) performHealthChecks() {
+	lb.mu.RLock() // Use read lock because we're only reading the slice here
+	satellites := make([]*SatelliteStatus, len(lb.satellites))
+	copy(satellites, lb.satellites) // Copy the slice to avoid locking during iteration
+	lb.mu.RUnlock()
 
+	var wg sync.WaitGroup
+	for _, satellite := range satellites {
+		wg.Add(1)
+		go func(sat *SatelliteStatus) {
+			defer wg.Done()
+			// Replace with actual health check logic (e.g., TCP ping or endpoint check)
+			conn, err := net.DialTimeout("tcp", sat.IP+":9050", 5*time.Second)
+			lb.mu.Lock() // Lock when modifying the satellite's data
+			if err != nil {
+				sat.Healthy = false
+				logrus.WithFields(logrus.Fields{"zeroTierIP": sat.IP, "error": err}).Error("Health check failed")
+			} else {
+				sat.Healthy = true
+				sat.LastHealthCheck = time.Now()
+				logrus.WithField("zeroTierIP", sat.IP).Info("Health check passed")
+				conn.Close()
+			}
+			lb.mu.Unlock()
+		}(satellite)
+	}
+	wg.Wait()
+}
+
+// scheduleHealthChecks schedules health checks to run at regular intervals.
+func (lb *LoadBalancer) scheduleHealthChecks(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			lb.performHealthChecks()
+		case <-lb.shutdownChan:
+			logrus.Info("Stopping scheduled health checks")
+			return
+		}
+	}
+}
+
+// Shutdown gracefully shuts down the LoadBalancer.
+func (lb *LoadBalancer) Shutdown(ctx context.Context) error {
+	close(lb.shutdownChan) // Signal health checks to stop
+	close(lb.connChan)     // Close the connections channel
+
+	done := make(chan struct{})
 	go func() {
-		logrus.WithField("loadBalancer", lb).Info("Current LoadBalancer before NextSatellite")        // Print the address of the LoadBalancer instance
-		logrus.WithField("satellites", lb.satellites).Info("Current satellites before NextSatellite") // Print the current list of satellites
-		zeroTierIP, err := lb.NextSatellite()
-		if err != nil {
-			logrus.WithField("error", err).Error("Failed to get next satellite")
-			return
-		}
-
-		logrus.WithField("zeroTierIP", zeroTierIP).Info("Dialing satellite")
-		satelliteConn, err := net.DialTimeout("tcp", zeroTierIP+":9050", 5*time.Second)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{"zeroTierIP": zeroTierIP, "port": "9050", "error": err}).Error("Failed to connect to satellite via ZeroTier IP")
-			return
-		}
-		defer satelliteConn.Close()
-
-		logrus.WithFields(logrus.Fields{"zeroTierIP": zeroTierIP, "port": "9050"}).Info("Connected to satellite")
-
-		// Copy data between the incoming connection and the satellite
-		logrus.WithField("zeroTierIP", zeroTierIP).Info("Copying data to satellite")
-		copyData(conn, satelliteConn)
-		logrus.WithField("zeroTierIP", zeroTierIP).Info("Copying data from satellite")
-		copyData(satelliteConn, conn)
+		lb.wg.Wait() // Wait for all connection handling to complete
+		close(done)
 	}()
+
+	select {
+	case <-done:
+		return nil // Shutdown completed
+	case <-ctx.Done():
+		return ctx.Err() // Shutdown timed out
+	}
+}
+
+// NextSatellite returns the satellite with the least active connections.
+func (lb *LoadBalancer) NextSatellite() (*SatelliteStatus, error) {
+	lb.mu.RLock() // Use read lock because we're only reading the data
+	defer lb.mu.RUnlock()
+
+	if len(lb.satellites) == 0 {
+		return nil, errors.New("no satellites registered")
+	}
+
+	var leastConnSatellite *SatelliteStatus
+	for _, satellite := range lb.satellites {
+		if !satellite.Healthy {
+			continue
+		}
+		if leastConnSatellite == nil || satellite.ActiveConns < leastConnSatellite.ActiveConns {
+			leastConnSatellite = satellite
+		}
+	}
+
+	if leastConnSatellite == nil {
+		return nil, errors.New("no healthy satellites available")
+	}
+
+	return leastConnSatellite, nil
+}
+
+// handleConnections handles incoming connections from the buffered channel.
+func (lb *LoadBalancer) handleConnections() {
+	for conn := range lb.connChan {
+		lb.wg.Add(1) // Increment the wait group counter
+		go func(c net.Conn) {
+			defer lb.wg.Done() // Decrement the wait group counter when done
+			lb.handleSingleConnection(c)
+		}(conn)
+	}
+	logrus.Info("Stopped handling connections")
+}
+
+// HandleConnection enqueues an incoming connection to the buffered channel.
+func (lb *LoadBalancer) HandleConnection(conn net.Conn) {
+	lb.connChan <- conn
+}
+
+// handleSingleConnection handles a single connection from the buffered channel.
+func (lb *LoadBalancer) handleSingleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	satellite, err := lb.NextSatellite()
+	if err != nil {
+		logrus.WithField("error", err).Error("Failed to get next satellite")
+		return
+	}
+
+	satelliteConn, err := net.DialTimeout("tcp", satellite.IP+":9050", 5*time.Second)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{"zeroTierIP": satellite.IP, "error": err}).Error("Failed to connect to satellite")
+		return
+	}
+	defer func() {
+		if err := satelliteConn.Close(); err != nil {
+			logrus.WithFields(logrus.Fields{"zeroTierIP": satellite.IP, "error": err}).Error("Failed to close satellite connection")
+		}
+	}()
+
+	lb.mu.Lock()
+	satellite.ActiveConns++
+	lb.mu.Unlock()
+
+	defer func() {
+		lb.mu.Lock()
+		satellite.ActiveConns--
+		lb.mu.Unlock()
+	}()
+
+	go copyData(conn, satelliteConn)
+	go copyData(satelliteConn, conn)
 }
 
 // copyData copies data between two connections.
 func copyData(dst net.Conn, src net.Conn) {
-	logrus.WithFields(logrus.Fields{
-		"dst": dst.RemoteAddr().String(),
-		"src": src.RemoteAddr().String(),
-	}).Info("Copying data")
-	defer dst.Close()
-	defer src.Close()
-
-	_, err := io.Copy(dst, src)
-	if err != nil {
+	if _, err := io.Copy(dst, src); err != nil {
 		logrus.WithField("error", err).Error("Failed to copy data between connections")
+	}
+	if err := dst.Close(); err != nil {
+		logrus.WithField("error", err).Error("Failed to close destination connection")
+	}
+	if err := src.Close(); err != nil {
+		logrus.WithField("error", err).Error("Failed to close source connection")
 	}
 }
